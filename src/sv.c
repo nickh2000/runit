c33135ef8f3bc2b982657b7249cb70c60e3a9b85
nickh2000 <nickhurley2000@gmail.com>
2025-05-09 17:51:11 -0400

    * sv.c: on 'up', send runsv the 'up' command properly to overwrite any pending 'down' command while runsv is sleeping

--- src/sv.c
+++ src/sv.c
@@ -251,7 +251,7 @@ int check(char *a) {
 int control(char *a) {
   if (svstatus_get() <= 0) return(-1);
   if (svstatus[17] == *a)
-    if (*a != 'd' || svstatus[18] == 1) return(0); /* once w/o term */
+    if (svstatus[18] == 1) return(0); /* once w/o term */
   if ((fd =open_write("supervise/control")) == -1) {
     if (errno != error_nodevice)
       warn("unable to open supervise/control");
